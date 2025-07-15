10b02463d1ae77fce395762e402c469c666353f3
rodri <rodri@RODRIGOVIDAURRE>
2025-07-15 19:32:48 -0300

    Adicionar arquivos de projeto.

---
+++ desafio_bar/Class1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Numerics;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace desafio_bar
+{
+    internal class Conta
+    {
+        public char Sexo;
+        public int Cerveja;
+        public int Espetinho;
+        public int Refrigerante; 
+
+        public double Consumo(int Cerveja, int Espetinho, int Refrigerante)
+        {
+            return Cerveja * 5.0 + Refrigerante * 3.0 + Espetinho * 7.0;
+        }
+
+        public double Couvert()
+        {
+            if (Consumo(Cerveja, Espetinho, Refrigerante) <= 30){
+                return 4;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        public double Entrada(char Sexo)
+        {
+            if (Sexo == 'f' || Sexo == 'F')
+            {
+                return 8;
+            }
+            else
+            {
+                return 10;
+            }
+        }
+
+        public double Total()
+        {
+            if (Consumo(Cerveja, Espetinho, Refrigerante) > 30)
+            {
+                return Consumo(Cerveja, Espetinho, Refrigerante) + Entrada(Sexo);
+            }
+            else
+            {
+                return Consumo(Cerveja, Espetinho, Refrigerante) + Entrada(Sexo) + 4;
+            }
+        }
+
+        public override string ToString()
+        {
+            return "Relatório: \nConsumo: " + Consumo(Cerveja, Espetinho, Refrigerante) + "\nCouvert: " + Couvert() + "\nIngresso: "
+                + Entrada(Sexo) + "\n\nValor a pagar: " + Total();
+        }
+    }
+}
---
+++ desafio_bar/Program.cs
@@ -0,0 +1,26 @@
+﻿using desafio_bar;
+
+Conta conta = new Conta();
+
+Console.WriteLine("Sexo: ");
+conta.Sexo = char.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de cervejas: ");
+conta.Cerveja = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de refrigerantes: ");
+conta.Refrigerante = int.Parse(Console.ReadLine());
+
+Console.WriteLine("Quantidade de espetinhos: ");
+conta.Espetinho = int.Parse(Console.ReadLine());
+
+conta.Consumo(conta.Cerveja, conta.Espetinho, conta.Refrigerante);
+
+conta.Couvert();
+
+conta.Entrada(conta.Sexo);
+
+conta.Total();
+
+Console.WriteLine(conta);
+
